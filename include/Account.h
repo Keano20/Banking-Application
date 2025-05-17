8d002eaaed07f48a0a8eae3633bce71fe45c7e6d
Keano Ricketts <keano.ricketts@yahoo.com>
2025-05-17 10:35:06 +0100

    Add Account base class header defining common account structure and behavior

--- include/Account.h
+++ include/Account.h
@@ -1,14 +1,24 @@
-//
-// Created by keano ricketts on 17/05/2025.
-//
-
 #ifndef ACCOUNT_H
 #define ACCOUNT_H
 
-
+#include <string>
 
 class Account {
+  protected:
+    int accountId;
+    std::string ownerName;
+    float balance;
 
+  public:
+    Account(int id, const std::string& owner, float initialBalance);
+    virtual ~Account() = default;
+
+    virtual void calculateInterest() = 0;
+    void deposit(float amount);
+    bool withdraw(float amount);
+    float getBalance() const;
+    int getId() const;
+    std::string getOwnerName() const;
 };
 
 
