6ef9f1f90ee9a4fa12403a532e360f56fd9b59d3
Keano Ricketts <keano.ricketts@yahoo.com>
2025-05-17 11:57:00 +0100

    Implement SavingsAccount and CurrentAccount with interest logic; add AccountFactory using Factory design pattern and test functionality in main.cpp

--- include/AccountFactory.h
+++ include/AccountFactory.h
@@ -1,14 +1,13 @@
-//
-// Created by keano ricketts on 17/05/2025.
-//
-
 #ifndef ACCOUNTFACTORY_H
 #define ACCOUNTFACTORY_H
 
-
+#include <memory>
+#include <string>
+#include "Account.h"
 
 class AccountFactory {
-
+  public:
+    static std::unique_ptr<Account> createAccount(const std::string& type, int id, const std::string& owner, float initialBalance);
 };
 
 
--- src/AccountFactory.cpp
+++ src/AccountFactory.cpp
@@ -1,5 +1,18 @@
-//
-// Created by keano ricketts on 17/05/2025.
-//
-
 #include "AccountFactory.h"
+#include "SavingsAccount.h"
+#include "CurrentAccount.h"
+#include <algorithm>
+
+std::unique_ptr<Account> AccountFactory::createAccount(const std::string& type, int id, const std::string& owner, float initialBalance) {
+  std::string lowercaseType = type;
+  std::transform(lowercaseType.begin(), lowercaseType.end(), lowercaseType.begin(), ::tolower);
+
+  if (lowercaseType == "savings") {
+    return std::make_unique<SavingsAccount>(id, owner, initialBalance);
+  }
+  if (lowercaseType == "current") {
+    return std::make_unique<CurrentAccount>(id, owner, initialBalance);
+  }else {
+    return nullptr;
+  }
+}
