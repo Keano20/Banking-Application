1584615ae88dd0648da162c1f4f1cc1286f4cdfb
Keano Ricketts <keano.ricketts@yahoo.com>
2025-05-17 11:00:17 +0100

    Implement Account.cpp with core account logic (deposit, withdraw, accessors) and update CMakeLists.txt to compile with separated source and include directories

--- src/Account.cpp
+++ src/Account.cpp
@@ -1,5 +1,34 @@
-//
-// Created by keano ricketts on 17/05/2025.
-//
-
 #include "Account.h"
+
+// Constructor
+Account::Account(int id, const std::string& owner, float initialBalance)
+    : accountId(id), ownerName(owner), balance(initialBalance) {}
+
+// Method for depositing money
+void Account::deposit (float amount) {
+    if (amount > 0) balance += amount;
+}
+
+// Method for withdrawing money
+bool Account::withdraw(float amount) {
+    if (amount > 0 && balance >= amount) {
+        balance -= amount;
+        return true;
+    }
+    return false;
+}
+
+// Returns the account balance
+float Account::getBalance() const {
+    return balance;
+}
+
+// Returns the account ID
+int Account::getId() const {
+    return accountId;
+}
+
+// Returns the account owners name
+std::string Account::getOwnerName() const {
+    return ownerName;
+}
